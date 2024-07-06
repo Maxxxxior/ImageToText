6664a62eda5a08c4832a9b957e3742163ea81849
MaxxxxiorS <MaxxxxiorSteam@wp.pl>
2024-07-06 15:25:29 +0200

    Improved version, now program iterates through 'Images' directory. Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ImageToText
-Software made in python for converting text found on image to text
+
+This Python software converts text found in images into text files.
+
+## How to Use
+
+1. Place your desired image containing visible text into the **Images** folder.
+2. Run the program. It will scan the **Images** folder, extract text from the images, and save the text into the **Text** folder.
+3. Each text file in the **Text** folder will be named after its corresponding image file with a `.txt` extension.
+
+## Supported Image Formats
+
+Images in the following formats are supported: `.png`, `.jpg`, `.jpeg`, `.bmp`, and `.gif`. Note that support for `.gif` images may vary.
+
+## Example Files
+
+In the repository, you'll find sample files for testing:
+
+- Images: `text.png`, `text2.png`
+- Text: `text.txt`, `text2.txt`
+
+Feel free to use these for testing purposes, but they are not required.
+
+## Note on Text Extraction
+
+- The program may encounter difficulties with fancy or elaborate fonts.
+- Text extraction from images may not always be accurate or complete.
--- imageToText.py
+++ imageToText.py
@@ -1,4 +1,4 @@
-import pyautogui
+import os
 from PIL import Image
 from pytesseract import *
 
@@ -6,12 +6,26 @@ try:
     from config import TESSERACT_PATH
 except ImportError:
     TESSERACT_PATH = None
-    
-pytesseract.tesseract_cmd = TESSERACT_PATH
 
-img_name = "text2"
-img = Image.open(f"Images/{img_name}.png")
+if TESSERACT_PATH:
+    pytesseract.tesseract_cmd = TESSERACT_PATH
 
-output = pytesseract.image_to_string(img)
+images_folder = "Images"
+text_folder = "Text"
 
-print(output)
+os.makedirs(text_folder, exist_ok = True)
+
+for img_name in os.listdir(images_folder):
+    if img_name.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
+        img_path = os.path.join(images_folder, img_name)
+        img = Image.open(img_path)
+        
+        output = pytesseract.image_to_string(img)
+        
+        text_file_name = os.path.splitext(img_name)[0] + ".txt"
+        text_file_path = os.path.join(text_folder, text_file_name)
+        
+        with open(text_file_path, "w", encoding = "utf-8") as text_file:
+            text_file.write(output)
+        
+        print(f"Processed {img_name} to {text_file_name}")
